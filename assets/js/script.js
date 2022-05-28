c1d89c670cf2e847bcbde0bacca3c61d3fb6cd2c
Rush <kriscookies20@gmail.com>
2022-05-28 15:31:58 -0700

    JS initial testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,27 @@
-var apiUrlCity = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+var apiUrlCity = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}';
 // var apiUrlForecast = 'https://api.openweathermap.org/data/2.5/onecall?';
 
-    // search temp, wind, humidity, uv index
+    // search temp, windspeed, humidity, uv index
     fetch(apiUrlCity).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 return response.json().then(function(data) {
-                    
-                })
-            })
-        }
-    })
+                    displayFacts(data);
+                    save(data.daily[0].humidity, data.daily[0].temp, data.daily[0].wind_speed, data.daily[0].uvi,)
+                });
+            });
+        };
+    });
 
+var displayFacts = function(data) {
+    console.log(displayFacts);
+    var weatherInfo = document.querySelector("#weather-info");
 
-
+    // display weather text facts - CITY
+    $("#city").text("");
+    // display weather text facts - UL LIST
+    $("weather-info").text("");
+}
 
 
 
