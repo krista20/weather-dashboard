ae9fb84a764c8660d40447d0b5726b84982ed079
Rush <kriscookies20@gmail.com>
2022-05-28 11:02:19 -0700

    js start

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var apiUrlCity = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+// var apiUrlForecast = 'https://api.openweathermap.org/data/2.5/onecall?';
+
+    // search temp, wind, humidity, uv index
+    fetch(apiUrlCity).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+
+            })
+        }
+    })
+
+
+
+
+
+
+    // var getRepoIssues = function(repo) {
+    //     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    
+    //     // make a get request to url
+    //     fetch(apiUrl).then(function(response) {
+    //     // request was successful
+    //     if (response.ok) {
+    //       response.json().then(function(data) {
+    //         displayIssues(data);
