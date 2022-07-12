59052a908889261ed7d0e20166ea538321459a1d
Rush <kriscookies20@gmail.com>
2022-07-11 19:10:37 -0700

    progress working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,24 @@
 // var apiUrlForecast = 'https://api.openweathermap.org/data/2.5/onecall?';
 var searchBtn = document.getElementById("get-weather");
 var inputEl = document.getElementById("search-city");
+var currentWeatherTitle = document.getElementById("title-name");
+var timeInfo = document.getElementById("info-date");
+var currentWeatherInfo = document.getElementById("info-current");
+var tempInfo = document.getElementById("info-temp");
+var windInfo = document.getElementById("info-wind");
+var humidityInfo = document.getElementById("info-humidity");
+var uvindexInfo = document.getElementById("info-uvindex");
 
 
 let key = "8c12b3ad070f07451d051e392d62dab6";
 let lang = 'en';
-let units = 'metric';
+let units = 'imperial';
 console.log("testing");
 // fetching weather data from openweathermap
 var getData = function(city) {
 
 // getWeatherApi
-var api = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + key;
+var api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=en&lat=lat&lon=lon&units=imperial&appid=" + key;
 // console.log(getData);
 console.log(api);
 
@@ -20,6 +27,20 @@ fetch(api).then(function(response){
     if(response.ok) {
         return response.json().then(function(data) {
             console.log(data);
+            currentWeatherTitle.textContent = data.name;
+            // timeInfo.textContent = data.timezone;
+                //date
+                var currentTime = new Date().toISOString(timeInfo.textContent = data.dt);
+                var month = currentTime.getMonth() + 1
+                var day = currentTime.getDate()
+                var year = currentTime.getFullYear()
+                var date = day + "/" + month + "/" + year
+                timeInfo.textContent = date;
+            tempInfo.textContent = data.main.temp;
+            windInfo.textContent = data.wind.speed;
+            humidityInfo.textContent = data.main.humidity;
+            uvindexInfo.textContent = data.uv;
+
         });
     };
 });
@@ -40,58 +61,4 @@ searchBtn.addEventListener("click", function(){
 
 })
 
-getData("Austin");
-// fetch(api) 
-// .then(response => response.json())
-// .then(data => {
-//   console.log('Success:', data);
-// })
-// .catch((error) => {
-//   console.error('Error:', error);
-// });
-
-
-
-
-
-
-
-
-    // search temp, windspeed, humidity, uv index
-    // fetch(api).then(function(response) {
-    //     if (response.ok) {
-    //         return response.json().then(function(data) {
-                
-    //         //     return response.json().then(function(data) {
-    //         //         displayFacts(data);
-    //         //         save(data.daily.humidity, data.daily[0].temp, data.daily.wind_speed, data.daily.uvi,)
-    //         //   console.log(data)  });
-    //         });
-    //     };
-    // });
-
-// var displayFacts = function(data) {
-//     console.log(displayFacts);
-//     var weatherInfo = document.querySelector("#weather-info");
-//     var humidityInfo = data.daily.humidity;
-//     var tempInfo = data.daily.temp;
-//     var windspeedInfo = data.daily.wind_speed;
-//     var uvInfo = data.daily.uvi;
-
-//     // display weather text facts - CITY
-//     $("#city").text(data);
-//     // display weather text facts - UL LIST
-//     $("weather-info").text(humidityInfo, tempInfo, windspeedInfo, uvInfo);
-// }
-
-
-
-    // var getRepoIssues = function(repo) {
-    //     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    
-    //     // make a get request to url
-    //     fetch(apiUrl).then(function(response) {
-    //     // request was successful
-    //     if (response.ok) {
-    //       response.json().then(function(data) {
-    //         displayIssues(data);
+// getData("Austin");
